560d3ba5d10746ccdd6de9a7b30410028744c7ef
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-01-09 23:01:44 -0600

    page view widget

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -6,9 +6,33 @@ class ScrollScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: const [Background(), MainContent()],
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color(0xff5EE8C5),
+              Color.fromARGB(255, 14, 108, 170),
+            ])),
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          children: const [Page1(), Page2()],
+        ),
       ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [Background(), MainContent()],
     );
   }
 }
@@ -53,5 +77,34 @@ class Background extends StatelessWidget {
         alignment: Alignment.topCenter,
         color: const Color(0xff30BAD6),
         child: const Image(image: AssetImage('assets/scroll-1.png')));
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff30BAD6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: (() => {}),
+            style: TextButton.styleFrom(
+                backgroundColor: const Color(0xff0098FA),
+                shape: const StadiumBorder()),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Bienvenido',
+                style: TextStyle(fontSize: 50, color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
