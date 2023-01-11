2819395a65b53b9044dc1d7b17a49f128ba3b479
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-01-11 10:58:39 -0600

    status bar y titulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:disenos_flutter_app/screens/basic_design.dart';
 import 'package:disenos_flutter_app/screens/home_screen.dart';
 import 'package:disenos_flutter_app/screens/scroll_design.dart';
-import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material app',
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:disenos_flutter_app/widgets/background.dart';
+import 'package:disenos_flutter_app/widgets/page_title.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,7 +9,25 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [BackGround()]),
+      body: Stack(children: [BackGround(), _HomeBody()]),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: const [
+          //  Titulos
+          PageTitle()
+        ],
+      ),
     );
   }
 }
